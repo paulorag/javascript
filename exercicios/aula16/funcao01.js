2ef55d08303eafdf1153f0950ba09bbce28b5c23
Paulo Roberto <89588460+paulorag@users.noreply.github.com>
2022-10-06 09:10:23 -0300

    att

---
+++ exercicios/aula16/funcao01.js
@@ -0,0 +1,13 @@
+function parImpa(n) {
+    if (n % 2 == 0) {
+        return 'Par!';
+    } else {
+        return 'Impar!';
+    }
+}
+
+/*
+let res = parImpa(223);
+console.log(res)
+*/
+console.log(parImpa(123));
